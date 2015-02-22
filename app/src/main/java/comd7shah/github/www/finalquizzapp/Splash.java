32441431c7efb9f6b50a5ec0eab1d6dcc6e7fe4f
Dhruti Shah <dhrutishah10@gmail.com>
2015-02-22 16:25:47 +0300

    added splash

---
+++ app/src/main/java/comd7shah/github/www/finalquizzapp/Splash.java
@@ -0,0 +1,40 @@
+package comd7shah.github.www.finalquizzapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by dhrutishah on 15-02-22.
+ */
+public class Splash extends Activity {
+
+    //Set the time the splash screen time will take
+
+    private static int splashInterval = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, Main.class);
+                startActivity(i);
+
+                this.finish();
+
+            }
+
+            private void finish() {
+
+            }
+
+        }, splashInterval);
+    }
+}
