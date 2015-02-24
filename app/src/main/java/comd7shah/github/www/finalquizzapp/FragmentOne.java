7a90245d01ca58d6352ced87d424ad96e08e231b
Dhruti Shah <dhrutishah10@gmail.com>
2015-02-24 17:29:16 +0300

    removed bg image

--- app/src/main/java/comd7shah/github/www/finalquizzapp/FragmentOne.java
+++ app/src/main/java/comd7shah/github/www/finalquizzapp/FragmentOne.java
@@ -3,7 +3,6 @@ package comd7shah.github.www.finalquizzapp;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +21,8 @@ public class FragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_one, container, false);
+
+
     }
 
     @Override
@@ -43,6 +44,8 @@ public class FragmentOne extends Fragment {
                 editor.commit();
             }
         });
+
+
 
     }
 
